c381441e84ce91cbb951aef1c659c7d65d89e4fa
zainahmedsharif <zain@DESKTOP-3JJA624.>
2024-12-08 09:35:25 -0330

    Added Template

--- application.py
+++ application.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import os
 from langchain.llms import OpenAI
@@ -10,39 +8,18 @@ from dotenv import load_dotenv
 import docx
 import fitz
 from flask import Flask, request, render_template, redirect, url_for
-
-
-# In[2]:
+from langchain.prompts import PromptTemplate
 
 
 load_dotenv()
 
 
-# In[3]:
-
 
 api_key = os.getenv("OPENAI_API_KEY")
 
 
-# In[ ]:
-
-
-
-
-
-# In[4]:
-
 
 llm = OpenAI(temperature=0.7, openai_api_key=api_key)
-
-
-# In[5]:
-
-
-from langchain.prompts import PromptTemplate
-
-
-# In[6]:
 
 
 summary_template = """ 
@@ -55,17 +32,11 @@ Summarize the text below briefly:
 
 """
 
-
-# In[7]:
-
-
 prompt = PromptTemplate(
     input_variables=["text"], 
     template=summary_template
 )
 
-
-# In[8]:
 
 
 def summarize_text(text):
@@ -80,27 +51,6 @@ def summarize_text(text):
     return response
 
 
-# In[9]:
-
-
-summarize_text("When interacting with the API some actions such as starting a Run and adding files to vector stores are asynchronous and take time to complete. The SDK includes helper functions which will poll the status until it reaches a terminal state and then return the resulting object. If an API method results in an action that could benefit from polling there will be a corresponding version of the method ending in '_and_poll'.")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[10]:
-
-
 def extract_text_from_pdf(pdf_path):
     text = ""
     with fitz.open(pdf_path) as pdf_document:
@@ -110,13 +60,8 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 
-# In[11]:
-
-
+#
 application = Flask(__name__)
-
-
-# In[12]:
 
 
 @application.route("/", methods=["GET", "POST"])
@@ -134,8 +79,8 @@ def index():
             text = ""
             if file_type == "pdf":
                 text = extract_text_from_pdf(file_path)
-            elif file_type == "docx":
-                text = extract_text_from_docx(file_path)
+#            elif file_type == "docx":
+#                text = extract_text_from_docx(file_path)
             elif file_type == "txt":
                 with open(file_path, "r", encoding="utf-8") as f:
                     text = f.read()
@@ -160,9 +105,6 @@ if __name__ == "__main__":
     if not os.path.exists("temp"):
         os.makedirs("temp")
     application.run(host='0.0.0.0',port=5001)
-
-
-# In[ ]:
 
 
 
